500fb6e1c54fe619943363acb1b92dc32d8549f8
Jeremy Olin <olinjeremy2@gmail.com>
2020-02-15 16:01:51 -0500

    Upload updated ActivityTracker files finished draft

--- ActivityTrackerShowcase.js
+++ ActivityTrackerShowcase.js
@@ -40,8 +40,61 @@ function removePeople(map, seconds, currentAttendance)
 
 function poll(map, currentAttendance)
 {
+  intervalTotal += currentAttendance;
   var seconds = calculateSeconds();
   currentAttendance = removePeople(map, seconds, currentAttendance);
-  console.log(seconds);
+  updateAvgAttendance(seconds);
   return currentAttendance;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+function updateAvgAttendance(time) {
+  if(time % 10 != 0) return;
+  
+  var date = new Date();
+  var slot = Math.floor(time/10);
+  var day = date.getMinutes();
+  
+  var entry = new Array();
+  if(database.has(day)) {
+    entry = database.get(day);
+  }
+  else {
+    for(var i = 0; i < 6; i++) {
+      entry.push([0,0]);
+    }
+  }
+  
+  var oldAvg = entry[slot][0];
+  var numRecords = entry[slot][1];
+  entry[slot] = [(oldAvg+(intervalTotal/10))/(numRecords+1), numRecords+1];
+  intervalTotal = 0;
+}
+
+function getAvgAttendance() {
+  var date = new Date();
+  var day = date.getMinutes();
+  var interv = Math.floor(calculateSeconds()/10);
+  //console.log(day + ", " + interv);
+  if(database.has(day)) {
+    var arrs = database.get(day);
+    var busyness = arrs[interv][0];
+    //console.log(busyness);
+    if(busyness >= 100) return "Very Busy";
+    else if(busyness >= 20) return "A Little Busy";
+    else return "Not Very Busy";
+  }
+  else return "No Data Available";
+}
+
+function fillDatabase() {
+  var numRuns = 2;
+  for(var min = 0; min < 60; min++) {
+    var arr = new Array();
+    for(var interval = 0; interval < 6; interval++) {
+      arr[interval] = [(Math.random()*110)+20, numRuns];
+    }
+    database.set(min, arr);
+  }
 }
